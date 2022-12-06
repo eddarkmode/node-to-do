303dc99d51ac815b5603bb7ac1a233de8a4a6fb0
Edgar Ramirez <edgar.rm9617@hotmail.com>
2022-12-06 11:03:01 -0600

    Interfaz para marcar o desmarcar tareas completadas

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -93,9 +93,52 @@ const listadoTareasBorrar = async( tareas = [] ) => {
     return id;
 }
 
+const confirmar = async (message) => {
+    const question = [{
+        type: 'confirm',
+        name: 'ok',
+        message
+    }];
+
+    const { ok } = await inquirer.prompt(question);
+
+    return ok;
+}
+
+const mostrarListadoChecklist = async( tareas = [] ) => {
+
+    const choices = tareas.map( (tarea, i) => {
+
+        const idx = `${ (i + 1) }.`.green;
+
+        return {
+            value: tarea.id,
+            name: `${ idx } ${ tarea.desc}`,
+            checked: ( tarea.completadoEn ) ? true : false, 
+        }
+    });
+
+    const preguntas = [
+        {
+            type: 'checkbox',
+            name: 'ids',
+            message: 'Seleccione',
+            choices
+        }
+    ];
+
+    const { ids } = await inquirer.prompt( preguntas );
+
+    //console.log(id);
+
+    return ids;
+}
+
 module.exports = {
     inquirerMenu,
     pausa,
     leerInput,
-    listadoTareasBorrar
+    listadoTareasBorrar,
+    confirmar,
+    mostrarListadoChecklist,
 }
